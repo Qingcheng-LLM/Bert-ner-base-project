9024e4ea0417cb3c0d21368c2526c12fe5333b6e
Qingcheng Chang <cqc19811855211@163.com>
2025-11-18 21:32:08 +0800

    优化了downloadmodel文件

--- downloadmodel.py
+++ downloadmodel.py
@@ -1,12 +1,7 @@
-import os
 from huggingface_hub import snapshot_download
 
-models = ["google-bert/bert-base-chinese", "hfl/chinese-bert-wwm"]
-root = os.path.dirname(__file__) # 获取当前脚本所在目录
-for rid in models:
-    out = os.path.join(root, rid.split("/")[-1])  # 拼接本地保存路径
-    snapshot_download(rid, local_dir=out, local_dir_use_symlinks=False, resume_download=True)
+snapshot_download(repo_id="google-bert/bert-base-chinese",local_dir = "pre_model/bert-base-chinese",ignore_patterns=["*.h5","flax_model.msgpack",""], local_dir_use_symlinks=False,resume_download=True)
+snapshot_download(repo_id="hfl/chinese-bert-wwm",local_dir = "pre_model/chinese-bert-wwm",ignore_patterns=["*.h5","flax_model.msgpack",""], local_dir_use_symlinks=False,resume_download=True)
 print("预训练权重已下载到BERT-NER/pre_model目录下对应子文件夹中")
-
 # https://huggingface.co/hfl/chinese-bert-wwm
 # https://huggingface.co/google-bert/bert-base-chinese
